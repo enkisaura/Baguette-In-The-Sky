13738fc97179a0b16a930c8637679ba165c9c0d4
EnkiSAURA <esaura@ikosconsulting.com>
2026-05-21 13:09:25 +0200

    [dev] Refactoring plot3d -> takes dataframe as input + enhanced lisibility

--- bits/src/plotter.py
+++ bits/src/plotter.py
@@ -16,37 +16,86 @@ import folium
 from folium.plugins import FastMarkerCluster
 import requests
 
+from bits.src.const import RE as earth_radius
 
-def plot3d(data):
-    """
-    Traceurs 3d de satellites
 
-    :param data: [liste_x_ecef, liste_y_ecef, liste_z_ecef, liste_noms]
-    :return:
+def plot3d(pd_gnss:pd.DataFrame, sv_pos_ecef_cols: tuple=("x_sv_m", "y_sv_m", "z_sv_m"),
+           sv_name_col: str | None = "sv_id",
+           rx_pos_ecef_cols: tuple | None =("x_rx_m", "y_rx_m", "z_rx_m"), plot_all_timestamps: bool=False):
     """
-    plt.figure(label="Satellites en orbites")
-    ax = plt.axes(projection="3d")
-    ax.set_xlim(-50000000, 50000000)
-    ax.set_ylim(-50000000, 50000000)
-    ax.set_zlim(-50000000, 50000000)
-    ax.set_aspect("equal", adjustable="box")
+    3D plot of GNSS satellites and receiver in ECEF coordinates.
+
+    :param pd_gnss: GNSS dataframe.
+    :param sv_pos_ecef_cols: Columns containing satellite ECEF coordinates.
+    :param sv_name_col: Satellite name column.
+    :param rx_pos_ecef_cols: Receiver ECEF coordinate columns.
+    :param plot_all_timestamps: If False, keeps only first occurrence of each satellite.
+    :return: ax
+    """
+    pd_gnss = pd_gnss.copy()
+
+    # Keep only one occurrence of each satellite
+    if not plot_all_timestamps and sv_name_col is not None:
+        pd_gnss = pd_gnss.drop_duplicates(subset=[sv_name_col], keep="first")
+
+    # Create figure
+    fig = plt.figure("3D plot of visible satellites")
+    ax = fig.add_subplot(111, projection="3d")
+    ax.set_box_aspect([1, 1, 1])
+    # Remove grid and panels
+    ax.grid(False)
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.set_axis_off()
+    # Find maximum altitude
+    pd_gnss["range_m"] = np.sqrt(pd_gnss[sv_pos_ecef_cols[0]]**2
+                                 + pd_gnss[sv_pos_ecef_cols[1]]**2
+                                 + pd_gnss[sv_pos_ecef_cols[2]]**2)
+    lim = pd_gnss["range_m"].max() + 100
+    ax.set_xlim(-lim, lim)
+    ax.set_ylim(-lim, lim)
+    ax.set_zlim(-lim, lim)
+
+    # Plot orientation
+    scale = 1e7
+    ax.quiver(0, 0, 0, scale, 0, 0, color="r")
+    ax.text(scale, 0, 0, "X")
+    ax.quiver(0, 0, 0, 0, scale, 0, color="g")
+    ax.text(0, scale, 0, "Y")
+    ax.quiver(0, 0, 0, 0, 0, scale, color="b")
+    ax.text(0, 0, scale, "Z")
 
     # Plot earth
-    u = np.linspace(0, 2 * np.pi, 20)
-    v = np.linspace(0, np.pi, 20)
-    x = 6378000 * np.outer(np.cos(u), np.sin(v))
-    y = 6378000 * np.outer(np.sin(u), np.sin(v))
-    z = 6378000 * np.outer(np.ones(np.size(u)), np.cos(v))
-    ax.plot_surface(x, y, z, color='b')
+    u = np.linspace(0, 2 * np.pi, 50)
+    v = np.linspace(0, np.pi, 50)
+    x = earth_radius * np.outer(np.cos(u), np.sin(v))
+    y = earth_radius * np.outer(np.sin(u), np.sin(v))
+    z = earth_radius * np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x, y, z, color="lightblue", alpha=0.5, linewidth=0)
 
-    # Plot data
-    ax.scatter3D(data[0], data[1], data[2], color='r', s=1)
+    # Plot satellite vehicles
+    ax.scatter(pd_gnss[sv_pos_ecef_cols[0]], pd_gnss[sv_pos_ecef_cols[1]], pd_gnss[sv_pos_ecef_cols[2]], color='r', s=1)
 
-    try:
-        for i in range(len(data[3])):
-            ax.text(data[0][i], data[1][i], data[2][i], data[3][i], size=5, zorder=1)
-    except:
-        print("Plot 3D : Noms inutilisables")
+    # Satellite labels
+    if sv_name_col is not None:
+        pd_gnss = pd_gnss.drop_duplicates(subset=[sv_name_col], keep="last") # Only keep one occurence of each SV
+        for _, row in pd_gnss.iterrows():
+            ax.text(row[sv_pos_ecef_cols[0]], row[sv_pos_ecef_cols[1]], row[sv_pos_ecef_cols[2]],
+                    str(row[sv_name_col]),
+                    size=8,)
+
+    # Plot receiver
+    if rx_pos_ecef_cols is not None:
+        if plot_all_timestamps:
+            ax.scatter(pd_gnss[rx_pos_ecef_cols[0]], pd_gnss[rx_pos_ecef_cols[1]], pd_gnss[rx_pos_ecef_cols[2]],
+                       color='r', s=1)
+        else:
+            ax.scatter(pd_gnss[rx_pos_ecef_cols[0]].iloc[0], pd_gnss[rx_pos_ecef_cols[1]].iloc[0],
+                       pd_gnss[rx_pos_ecef_cols[2]].iloc[0], color='r', s=1)
+
+
+    return ax
 
 
 def plot(pd_gnss_pvt: pd.DataFrame, m:folium.map=None, plot_name: str = "Plot", plot_rail=True) -> folium.map:
